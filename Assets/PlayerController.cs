65be49de764dff48aa8a6a4e053985357c0ab151
ec19402 <ec19402@qmul.ac.uk>
2020-10-27 20:16:25 +0000

    Setting up github
    Setting up github

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float walkSpeed;
 
+    public Camera camera;
+
     void Start()
     {
         
@@ -13,11 +15,29 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        movePlayer();
+        lookAtMouse();
+    }
+
+    void LateUpdate()
+    {
+        camera.transform.position = transform.position + new Vector3(0.0f, 0.0f, -10.0f);
+    }
+
+    void movePlayer()
+    {
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
 
         float normSpeed = Mathf.Max(Mathf.Abs(vertical), Mathf.Abs(horizontal));
 
-        transform.position += new Vector3(horizontal, vertical, 0.0f).normalized * Time.deltaTime * normSpeed * walkSpeed;
+        transform.position += new Vector3(horizontal, vertical, 0.0f).normalized * normSpeed * Time.deltaTime * walkSpeed;
+    }
+
+    void lookAtMouse()
+    {
+        Vector3 mouse = Input.mousePosition;
+
+        Debug.Log(mouse);
     }
 }
