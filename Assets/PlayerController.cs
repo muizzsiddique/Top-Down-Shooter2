d801d783438bc17952fc980c8f768e013c4903fc
Muizz Siddique <muizzsiddique@gmail.com>
2020-10-27 19:03:20 +0000

    Movable Player - Minimal
    Player just walks around

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        float normSpeed = Mathf.Max(Mathf.Abs(vertical), Mathf.Abs(horizontal));
+
+        transform.position += new Vector3(horizontal, vertical, 0.0f).normalized * Time.deltaTime * normSpeed * walkSpeed;
+    }
+}
